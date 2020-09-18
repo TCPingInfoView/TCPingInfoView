0976819eeb20b0e434befdb6fb3381970b218e65
Bruce Wayne <admin@bige0.com>
2020-09-18 22:01:26 +0800

    Add a extension for reflection

---
+++ TCPingInfoView/Utils/New.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace TCPingInfoView.Utils
+{
+	public static class New<T> where T : new()
+	{
+		public static readonly Func<T> Instance = Expression.Lambda<Func<T>>(Expression.New(typeof(T))).Compile();
+	}
+}
