425c8e6dddc59d456f9dfd87c6937457287f5229
Bruce Wayne <admin@bige0.com>
2020-09-23 17:29:17 +0800

    Add ReactiveChromelessWindow

---
+++ TCPingInfoView/Views/ReactiveChromelessWindow.cs
@@ -0,0 +1,25 @@
+﻿using ReactiveUI;
+using Syncfusion.Windows.Shared;
+using System.Windows;
+
+namespace TCPingInfoView.Views
+{
+	public class ReactiveChromelessWindow<TViewModel> : ChromelessWindow, IViewFor<TViewModel> where TViewModel : class
+	{
+		public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(nameof(ViewModel), typeof(TViewModel), typeof(ReactiveWindow<TViewModel>), new PropertyMetadata(null));
+
+		public TViewModel BindingRoot => ViewModel;
+
+		public TViewModel ViewModel
+		{
+			get => (TViewModel)GetValue(ViewModelProperty);
+			set => SetValue(ViewModelProperty, value);
+		}
+
+		object IViewFor.ViewModel
+		{
+			get => ViewModel;
+			set => ViewModel = (TViewModel)value;
+		}
+	}
+}
