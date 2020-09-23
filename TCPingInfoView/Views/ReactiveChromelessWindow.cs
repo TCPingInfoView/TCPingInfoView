7d3140fbb9a1aefd4f7304214d98d3a87a439b47
Bruce Wayne <admin@bige0.com>
2020-09-23 17:51:44 +0800

    Use SfChromelessWindow

--- TCPingInfoView/Views/ReactiveChromelessWindow.cs
+++ TCPingInfoView/Views/ReactiveChromelessWindow.cs
@@ -1,10 +1,10 @@
 ﻿using ReactiveUI;
-using Syncfusion.Windows.Shared;
+using Syncfusion.Windows.Controls;
 using System.Windows;
 
 namespace TCPingInfoView.Views
 {
-	public class ReactiveChromelessWindow<TViewModel> : ChromelessWindow, IViewFor<TViewModel> where TViewModel : class
+	public class ReactiveChromelessWindow<TViewModel> : SfChromelessWindow, IViewFor<TViewModel> where TViewModel : class
 	{
 		public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(nameof(ViewModel), typeof(TViewModel), typeof(ReactiveWindow<TViewModel>), new PropertyMetadata(null));
 
